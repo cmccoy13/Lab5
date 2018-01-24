4fc37fc1cd308d750247b2f21710098582742c69
Christopher McCoy <35251302+cmccoy13@users.noreply.github.com>
2018-01-24 11:00:39 -0800

    Add files via upload

---
+++ lab going3D/src/main.cpp
@@ -0,0 +1,301 @@
+/*
+ZJ Wood CPE 471 Lab 3 base code
+*/
+
+#include <iostream>
+#include <glad/glad.h>
+
+#include "GLSL.h"
+#include "Program.h"
+#include "MatrixStack.h"
+
+#include "WindowManager.h"
+
+// value_ptr for glm
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+
+class Application : public EventCallbacks
+{
+
+public:
+
+	WindowManager * windowManager = nullptr;
+
+	// Our shader program
+	std::shared_ptr<Program> prog;
+
+	// Contains vertex information for OpenGL
+	GLuint VertexArrayID;
+
+	// Data necessary to give our box to OpenGL
+	GLuint VertexBufferID, VertexColorIDBox, IndexBufferIDBox;
+
+	void keyCallback(GLFWwindow *window, int key, int scancode, int action, int mods)
+	{
+		if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		{
+			glfwSetWindowShouldClose(window, GL_TRUE);
+		}
+	}
+
+	// callback for the mouse when clicked move the triangle when helper functions
+	// written
+	void mouseCallback(GLFWwindow *window, int button, int action, int mods)
+	{
+		double posX, posY;
+		float newPt[2];
+		if (action == GLFW_PRESS)
+		{
+			glfwGetCursorPos(window, &posX, &posY);
+			std::cout << "Pos X " << posX <<  " Pos Y " << posY << std::endl;
+
+			//change this to be the points converted to WORLD
+			//THIS IS BROKEN< YOU GET TO FIX IT - yay!
+			newPt[0] = 0;
+			newPt[1] = 0;
+
+			std::cout << "converted:" << newPt[0] << " " << newPt[1] << std::endl;
+			glBindBuffer(GL_ARRAY_BUFFER, VertexBufferID);
+			//update the vertex array with the updated points
+			glBufferSubData(GL_ARRAY_BUFFER, sizeof(float)*6, sizeof(float)*2, newPt);
+			glBindBuffer(GL_ARRAY_BUFFER, 0);
+		}
+	}
+
+	//if the window is resized, capture the new size and reset the viewport
+	void resizeCallback(GLFWwindow *window, int in_width, int in_height)
+	{
+		//get the window size - may be different then pixels for retina
+		int width, height;
+		glfwGetFramebufferSize(window, &width, &height);
+		glViewport(0, 0, width, height);
+	}
+
+	/*Note that any gl calls must always happen after a GL state is initialized */
+	void initGeom()
+	{
+		//generate the VAO
+		glGenVertexArrays(1, &VertexArrayID);
+		glBindVertexArray(VertexArrayID);
+
+		//generate vertex buffer to hand off to OGL
+		glGenBuffers(1, &VertexBufferID);
+		//set the current state to focus on our vertex buffer
+		glBindBuffer(GL_ARRAY_BUFFER, VertexBufferID);
+
+		GLfloat cube_vertices[] = {
+			// front
+			-1.0, -1.0,  1.0,
+			1.0, -1.0,  1.0,
+			1.0,  1.0,  1.0,
+			-1.0,  1.0,  1.0,
+			// back
+			-1.0, -1.0, -1.0,
+			1.0, -1.0, -1.0,
+			1.0,  1.0, -1.0,
+			-1.0,  1.0, -1.0,
+		};
+		//make it a bit smaller
+		for (int i = 0; i < 24; i++)
+			cube_vertices[i] *= 0.5;
+		//actually memcopy the data - only do this once
+		glBufferData(GL_ARRAY_BUFFER, sizeof(cube_vertices), cube_vertices, GL_DYNAMIC_DRAW);
+
+		//we need to set up the vertex array
+		glEnableVertexAttribArray(0);
+		//key function to get up how many elements to pull out at a time (3)
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*) 0);
+
+		//color
+		GLfloat cube_colors[] = {
+			// front colors
+			1.0, 0.0, 0.0,
+			0.0, 1.0, 0.0,
+			0.0, 0.0, 1.0,
+			1.0, 1.0, 1.0,
+			// back colors
+			1.0, 0.0, 0.0,
+			0.0, 1.0, 0.0,
+			0.0, 0.0, 1.0,
+			1.0, 1.0, 1.0,
+		};
+		glGenBuffers(1, &VertexColorIDBox);
+		//set the current state to focus on our vertex buffer
+		glBindBuffer(GL_ARRAY_BUFFER, VertexColorIDBox);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(cube_colors), cube_colors, GL_STATIC_DRAW);
+		glEnableVertexAttribArray(1);
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+
+		glGenBuffers(1, &IndexBufferIDBox);
+		//set the current state to focus on our vertex buffer
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IndexBufferIDBox);
+		GLushort cube_elements[] = {
+			// front
+			0, 1, 2,
+			2, 3, 0,
+			// top
+			1, 5, 6,
+			6, 2, 1,
+			// back
+			7, 6, 5,
+			5, 4, 7,
+			// bottom
+			4, 0, 3,
+			3, 7, 4,
+			// left
+			4, 5, 1,
+			1, 0, 4,
+			// right
+			3, 2, 6,
+			6, 7, 3,
+		};
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(cube_elements), cube_elements, GL_STATIC_DRAW);
+
+
+
+		glBindVertexArray(0);
+
+	}
+
+	//General OGL initialization - set OGL state here
+	void init(const std::string& resourceDirectory)
+	{
+		GLSL::checkVersion();
+
+		// Set background color.
+		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+		// Enable z-buffer test.
+		glEnable(GL_DEPTH_TEST);
+
+		// Initialize the GLSL program.
+		prog = std::make_shared<Program>();
+		prog->setVerbose(true);
+		prog->setShaderNames(resourceDirectory + "/shader_vertex.glsl", resourceDirectory + "/shader_fragment.glsl");
+		prog->init();
+		prog->addUniform("P");
+		prog->addUniform("V");
+		prog->addUniform("M");
+		prog->addAttribute("vertPos");
+		prog->addAttribute("vertColor");
+	}
+
+
+	/****DRAW
+	This is the most important function in your program - this is where you
+	will actually issue the commands to draw any geometry you have set up to
+	draw
+	********/
+	void render()
+	{
+		// Get current frame buffer size.
+		int width, height;
+		glfwGetFramebufferSize(windowManager->getHandle(), &width, &height);
+		float aspect = width/(float)height;
+		glViewport(0, 0, width, height);
+
+		// Clear framebuffer.
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		// Create the matrix stacks - please leave these alone for now
+		
+		glm::mat4 V, M, P; //View, Model and Perspective matrix
+		V = glm::mat4(1);
+		M = glm::mat4(1);
+		// Apply orthographic projection....
+		P = glm::ortho(-1 * aspect, 1 * aspect, -1.0f, 1.0f, -2.0f, 100.0f);		
+		if (width < height)
+			{
+			P = glm::ortho(-1.0f, 1.0f, -1.0f / aspect,  1.0f / aspect, -2.0f, 100.0f);
+			}
+		// ...but we overwrite it (optional) with a perspective projection.
+		P = glm::perspective((float)(3.14159 / 4.), (float)((float)width/ (float)height), 0.1f, 1000.0f); //so much type casting... GLM metods are quite funny ones
+
+		//animation with the model matrix:
+		static float w = 0.0;
+		w += 0.01;//rotation angle
+		glm::mat4 RotateX = glm::rotate(glm::mat4(1.0f), w, glm::vec3(-1.0f, 1.0f, 0.0f));
+		glm::mat4 ScaleY = glm::scale(glm::mat4(1.0f), glm::vec3(0.2f, 1.0f, 1.0f));
+		glm::mat4 Trans = glm::translate(glm::mat4(1.0f), glm::vec3(-0.4f, 0.0f, -3));
+		M = Trans * ScaleY;
+
+		// Draw the box using GLSL.
+		prog->bind();
+
+		//send the matrices to the shaders
+		glUniformMatrix4fv(prog->getUniform("P"), 1, GL_FALSE, &P[0][0]);
+		glUniformMatrix4fv(prog->getUniform("V"), 1, GL_FALSE, &V[0][0]);
+		glUniformMatrix4fv(prog->getUniform("M"), 1, GL_FALSE, &M[0][0]);
+
+		glBindVertexArray(VertexArrayID);
+		//actually draw from vertex 0, 3 vertices
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IndexBufferIDBox);
+		glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_SHORT, (void*)0); //Left H
+
+		Trans = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -3));
+		M = Trans * ScaleY;
+		glUniformMatrix4fv(prog->getUniform("M"), 1, GL_FALSE, &M[0][0]);
+		glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_SHORT, (void*)0); //Right H
+
+		Trans = glm::translate(glm::mat4(1.0f), glm::vec3(-0.2f, 0.0f, -3));
+		ScaleY = glm::scale(glm::mat4(1.0f), glm::vec3(0.8f, 0.2f, 1.0f));
+		w = -3.141592769/6;
+		RotateX = glm::rotate(glm::mat4(1.0f), w, glm::vec3(0.0f, 0.0f, 1.0f));
+		M = Trans * RotateX * ScaleY;
+		glUniformMatrix4fv(prog->getUniform("M"), 1, GL_FALSE, &M[0][0]);
+		glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_SHORT, (void*)0); //H Cross
+
+		Trans = glm::translate(glm::mat4(1.0f), glm::vec3(0.6f, 0.0f, -3));
+		ScaleY = glm::scale(glm::mat4(1.0f), glm::vec3(0.2f, 1.0f, 1.0f));
+		M = Trans * ScaleY;
+		glUniformMatrix4fv(prog->getUniform("M"), 1, GL_FALSE, &M[0][0]);
+		glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_SHORT, (void*)0); //I
+
+		glBindVertexArray(0);
+
+		prog->unbind();
+
+	}
+
+};
+//******************************************************************************************
+int main(int argc, char **argv)
+{
+	std::string resourceDir = "../resources"; // Where the resources are loaded from
+	if (argc >= 2)
+	{
+		resourceDir = argv[1];
+	}
+
+	Application *application = new Application();
+
+	/* your main will always include a similar set up to establish your window
+		and GL context, etc. */
+	WindowManager * windowManager = new WindowManager();
+	windowManager->init(640, 480);
+	windowManager->setEventCallbacks(application);
+	application->windowManager = windowManager;
+
+	/* This is the code that will likely change program to program as you
+		may need to initialize or set up different data and state */
+	// Initialize scene.
+	application->init(resourceDir);
+	application->initGeom();
+
+	// Loop until the user closes the window.
+	while(! glfwWindowShouldClose(windowManager->getHandle()))
+	{
+		// Render scene.
+		application->render();
+
+		// Swap front and back buffers.
+		glfwSwapBuffers(windowManager->getHandle());
+		// Poll for and process events.
+		glfwPollEvents();
+	}
+
+	// Quit program.
+	windowManager->shutdown();
+	return 0;
+}
